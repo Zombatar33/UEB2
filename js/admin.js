1a1b4a5c6d9efe5ced0a7ab8a02daa441845b46c
Zombatar33 <ageeadebied@gmail.com>
2022-06-07 21:10:03 +0200

    Implemented Add fully and did half assignment

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,43 @@
+let students = [
+    {
+        "id": "577868",
+        "first_name": "Adam",
+        "last_name": "Ebied",
+        "birthday": new Date('2002-11-04'),
+        "gender": "Male",
+        "department": "Applied Computer Science",
+        "email": "s0577868@htw-berlin.de",
+        "join_date": new Date('2015-11-30')
+    },
+    {
+        "id": "578838",
+        "first_name": "Bruno",
+        "last_name": "Braun",
+        "birthday": new Date('2001-05-20'),
+        "gender": "Male",
+        "department": "Applied Computer Science",
+        "email": "s0578838htw-berlin.de",
+        "join_date": new Date('2015-04-26')
+    }
+]
+
+let staff = [
+    {
+        "id": "912381",
+        "first_name": "John",
+        "last_name": "Doe",
+        "birthday": new Date('1994-04-11'),
+        "gender": "Male",
+        "department": "Applied Computer Science",
+        "email": "j.doe@htw-berlin.de",
+        "join_date": new Date('2015-02-04')
+    }
+]
+
+var student_mode = true;
+
 function manageStudents() {
+    student_mode = true;
     console.log("Students");
 
     // Clears Table
@@ -16,137 +55,237 @@ function manageStudents() {
     h3.innerHTML = "Last Name";
 
     var h4 = document.createElement("th");
-    h4.innerHTML = "Department";
+    h4.innerHTML = "Birthday";
 
     var h5 = document.createElement("th");
-    h5.innerHTML = "E-Mail";
+    h5.innerHTML = "Gender";
+
+    var h6 = document.createElement("th");
+    h6.innerHTML = "Department";
+
+    var h7 = document.createElement("th");
+    h7.innerHTML = "E-Mail";
+
+    var h8 = document.createElement("th");
+    h8.innerHTML = "Join Date";
 
     header.appendChild(h1);
     header.appendChild(h2);
     header.appendChild(h3);
     header.appendChild(h4);
     header.appendChild(h5);
+    header.appendChild(h6);
+    header.appendChild(h7);
+    header.appendChild(h8);
 
     table.appendChild(header);
 
     // Replace with iterator over json file? database!
-    for (var i = 0; i < 3; i++) {
+    students.forEach(student => {
         var row = document.createElement("tr");
         // Student ID
         var d1 = document.createElement("td");
-        d1.innerHTML = "577868";
+        d1.innerHTML = student.id;
         // Student First Name
         var d2 = document.createElement("td");
-        d2.innerHTML = "Adam";
+        d2.innerHTML = student.first_name;
         // Student Last Name
         var d3 = document.createElement("td");
-        d3.innerHTML = "Ebied";
-        // Student Department
+        d3.innerHTML = student.last_name;
+        // Student Birthday
         var d4 = document.createElement("td");
-        d4.innerHTML = "Applied Computer Science";
-        // Student E-Mail
+        d4.innerHTML = student.birthday.toLocaleDateString('de-de', { year:"numeric", month:"numeric", day:"numeric"})
+        // Student Gender
         var d5 = document.createElement("td");
-        d5.innerHTML = "s0577868@htw-berlin.de";
+        d5.innerHTML = student.gender;
+        // Student Department
+        var d6 = document.createElement("td");
+        d6.innerHTML = student.department;
+        // Student E-Mail
+        var d7 = document.createElement("td");
+        d7.innerHTML = student.email;
+        // Student Join Date
+        var d8 = document.createElement("td");
+        d8.innerHTML = student.join_date.toLocaleDateString('de-de', { year:"numeric", month:"numeric", day:"numeric"})
 
         row.appendChild(d1);
         row.appendChild(d2);
         row.appendChild(d3);
         row.appendChild(d4);
         row.appendChild(d5);
+        row.appendChild(d6);
+        row.appendChild(d7);
+        row.appendChild(d8);
 
         table.appendChild(row);
-    }
+    });
 }
 
 function manageStaff() {
+    student_mode = false;
+
     console.log("Staff");
 
-        // Clears Table
-        var table = document.getElementById("targettable");
-        table.innerHTML = ""
+    // Clears Table
+    var table = document.getElementById("targettable");
+    table.innerHTML = ""
     
-        var header = document.createElement("tr");
-        var h1 = document.createElement("th");
-        h1.innerHTML = "Staff ID";
-    
-        var h2 = document.createElement("th");
-        h2.innerHTML = "First Name";
-    
-        var h3 = document.createElement("th");
-        h3.innerHTML = "Last Name";
-    
-        var h4 = document.createElement("th");
-        h4.innerHTML = "Department";
-    
-        var h5 = document.createElement("th");
-        h5.innerHTML = "E-Mail";
-    
-        header.appendChild(h1);
-        header.appendChild(h2);
-        header.appendChild(h3);
-        header.appendChild(h4);
-        header.appendChild(h5);
-    
-        table.appendChild(header);
-    
-        // Replace with iterator over json file? database!
-        for (var i = 0; i < 3; i++) {
-            var row = document.createElement("tr");
-            // Student ID
-            var d1 = document.createElement("td");
-            d1.innerHTML = "192392";
-            // Student First Name
-            var d2 = document.createElement("td");
-            d2.innerHTML = "John";
-            // Student Last Name
-            var d3 = document.createElement("td");
-            d3.innerHTML = "Doe";
-            // Student Department
-            var d4 = document.createElement("td");
-            d4.innerHTML = "Applied Computer Science";
-            // Student E-Mail
-            var d5 = document.createElement("td");
-            d5.innerHTML = "john.doe@staff.htw-berlin.de";
-    
-            row.appendChild(d1);
-            row.appendChild(d2);
-            row.appendChild(d3);
-            row.appendChild(d4);
-            row.appendChild(d5);
-    
-            table.appendChild(row);
-        }
+    var header = document.createElement("tr");
+    var h1 = document.createElement("th");
+    h1.innerHTML = "Student ID";
+
+    var h2 = document.createElement("th");
+    h2.innerHTML = "First Name";
+
+    var h3 = document.createElement("th");
+    h3.innerHTML = "Last Name";
+
+    var h4 = document.createElement("th");
+    h4.innerHTML = "Birthday";
+
+    var h5 = document.createElement("th");
+    h5.innerHTML = "Gender";
+
+    var h6 = document.createElement("th");
+    h6.innerHTML = "Department";
+
+    var h7 = document.createElement("th");
+    h7.innerHTML = "E-Mail";
+
+    var h8 = document.createElement("th");
+    h8.innerHTML = "Join Date";
+
+    header.appendChild(h1);
+    header.appendChild(h2);
+    header.appendChild(h3);
+    header.appendChild(h4);
+    header.appendChild(h5);
+    header.appendChild(h6);
+    header.appendChild(h7);
+    header.appendChild(h8);
+
+    table.appendChild(header);
+
+    // Replace with iterator over json file? database!
+    staff.forEach(staff => {
+        var row = document.createElement("tr");
+        // Staff ID
+        var d1 = document.createElement("td");
+        d1.innerHTML = staff.id;
+        // Staff First Name
+        var d2 = document.createElement("td");
+        d2.innerHTML = staff.first_name;
+        // Staff Last Name
+        var d3 = document.createElement("td");
+        d3.innerHTML = staff.last_name;
+        // Staff Birthday
+        var d4 = document.createElement("td");
+        d4.innerHTML = staff.birthday.toLocaleDateString('de-de', { year:"numeric", month:"numeric", day:"numeric"})
+        // Staff Gender
+        var d5 = document.createElement("td");
+        d5.innerHTML = staff.gender;
+        // Staff Department
+        var d6 = document.createElement("td");
+        d6.innerHTML = staff.department;
+        // Staff E-Mail
+        var d7 = document.createElement("td");
+        d7.innerHTML = staff.email;
+        // Staff Join Date
+        var d8 = document.createElement("td");
+        d8.innerHTML = staff.join_date.toLocaleDateString('de-de', { year:"numeric", month:"numeric", day:"numeric"})
+
+        row.appendChild(d1);
+        row.appendChild(d2);
+        row.appendChild(d3);
+        row.appendChild(d4);
+        row.appendChild(d5);
+        row.appendChild(d6);
+        row.appendChild(d7);
+        row.appendChild(d8);
+
+        table.appendChild(row);
+    });
 }
+
+document.addEventListener('readystatechange', event => {
+    switch (document.readyState) {
+      case "loading":
+        console.log("document.readyState: ", document.readyState,
+         `- The document is still loading.`
+         );
+        break;
+      case "interactive":
+        console.log("document.readyState: ", document.readyState, 
+          `- The document has finished loading DOM. `,
+          `- "DOMContentLoaded" event`
+          );
+        break;
+      case "complete":
+        console.log("document.readyState: ", document.readyState, 
+          `- The page DOM with Sub-resources are now fully loaded. `,
+          `- "load" event`
+          );
+          manageStudents();
+        break;
+    }
+});
 
 function handleAdd() {
     console.log("Add");
 
     var table = document.getElementById("targettable");
 
-    var row = document.createElement("tr");
-    // Student ID
-    var d1 = document.createElement("td");
-    d1.innerHTML = "192392";
-    // Student First Name
-    var d2 = document.createElement("td");
-    d2.innerHTML = "Added";
-    // Student Last Name
-    var d3 = document.createElement("td");
-    d3.innerHTML = "Entity";
-    // Student Department
-    var d4 = document.createElement("td");
-    d4.innerHTML = "Applied Computer Science";
-    // Student E-Mail
-    var d5 = document.createElement("td");
-    d5.innerHTML = "added-entity@gmail.com";
+    var object = {
+        "id": document.getElementById("id").value,
+        "first_name": document.getElementById("fname").value,
+        "last_name": document.getElementById("lname").value,
+        "birthday": new Date(document.getElementById("bday").value),
+        "gender": document.getElementById("gender").value,
+        "department": document.getElementById("department").value,
+        "email": document.getElementById("email").value,
+        "join_date": new Date(document.getElementById("joindate").value)
+    }
 
-    row.appendChild(d1);
-    row.appendChild(d2);
-    row.appendChild(d3);
-    row.appendChild(d4);
-    row.appendChild(d5);
+    if (object.id == "" || object.first_name == "" || object.last_name == ""|| object.birthday == "" || object.gender == "" || object.email == "" || object.join_date == "") {
+        object = null;
+        window.alert("All fields must be filled out")
+        return;
+    }
 
-    table.appendChild(row);
+    var time_dif = Math.abs(new Date().getTime() - object.birthday.getTime());
+    var year_dif = time_dif / (1000 * 3600 * 24 * 365);
+
+    if (year_dif < 17 || year_dif > 60) {
+        object = null;
+        window.alert("Invalid DOB")
+        return;
+    }
+
+    if (object.join_date.getUTCFullYear() != "2015") {
+        object = null;
+        window.alert("Join Date must be from 2015")
+        return;
+    }
+
+    if (student_mode) {
+        if (students.map(s => s.id).indexOf(object.id) < 0) {
+            students.push(object);
+        }else {
+            window.alert("Duplicate Student ID")
+            return;
+        }
+    
+        manageStudents();
+    }else {
+        if (staff.map(s => s.id).indexOf(object.id) < 0) {
+            staff.push(object);
+        }else {
+            window.alert("Duplicate Staff ID")
+            return;
+        }
+    
+        manageStaff();
+    }
 }
 
 function handleUpdate() {
